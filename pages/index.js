c974323635969bf214e6629626fa6dbf783278e3
altmatics <7942069+altmatics@users.noreply.github.com>
2021-05-26 00:58:35 +0900

    added title

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Link from 'next/link'
 const Index = () => {
     return(
         <>
-        <div>Welcome to Next.js!</div>
+        <title>GitHub Pages test</title>
+        <div>test page</div>
         <Link href="/linkSample">
             <a>link Sample Page</a>
         </Link>
